ab011eadfb9aa7f56dc9850cce3a8f9b7f27bf43
mr-robot-007 <anujgusain108@gmail.com>
2023-10-26 22:37:41 +0530

    test commit

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -3,7 +3,7 @@ function StartScreen({numQuestions,dispatch}) {
         <div className="start">
             <h2>Welcome to the react quiz</h2>
             <h3>{numQuestions} questions to test your react mastery </h3>
-            <button className="btn btn-ui" onClick={() =>dispatch({type:"start"})}>Let's Start</button>
+            <button className="btn btn-ui" onClick={() =>dispatch({type:"start"})}>Let's Start!</button>
         </div>
     )
 }
